e4938b727212773a1dcfefc7d8fce22d2a8c2a5e
gurman2912 <w0830963@myscc.ca>
2023-10-12 15:46:03 -0400

    add helloworld controler and test it

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcDoll.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+       {
+           return "This is my default action...";
+       }
+        /*public IActionResult Index()
+         {
+             return View();
+         }*/
+
+
+         // 
+         // GET: /HelloWorld/Welcome/ 
+
+         public string Welcome()
+         {
+             return "This is the Welcome action method...";
+         }
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        
+
+    }
+}
