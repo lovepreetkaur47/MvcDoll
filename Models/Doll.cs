003b3c4581a846ddf4ce9a6952caea39905b214c
w0832389 <W0824592@myscc.ca>
2023-10-12 23:16:48 -0400

    validation done

--- Models/Doll.cs
+++ Models/Doll.cs
@@ -7,16 +7,30 @@ namespace MvcDoll.Models
     public class Doll
     {
         public int Id { get; set; }
+        [StringLength(100, MinimumLength = 4)]
+        [Required]
         public string Name { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(35)]
         public string Type { get; set; }
+
 
         [Display(Name = "Lauch Date")]
         [DataType(DataType.Date)]
         public DateTime LaunchDate { get; set; }
+
+        [StringLength(100, MinimumLength = 4)]
+        [Required]
         public string Color { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(6)]
+        [Required]
         public string Rating { get; set; }
 
 
